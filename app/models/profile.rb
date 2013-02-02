74cb056573035555899544f2e83b194cb666a5e7
Blair Anderson <blair81@gmail.com>
2013-02-02 00:07:08 -0700

    added linked in to the form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,3 @@
 class Profile < ActiveRecord::Base
-  attr_accessible :email, :github, :name, :phone, :text_editor, :twitter
+  attr_accessible :email, :github, :name, :phone, :text_editor, :twitter, :linkedin
 end
