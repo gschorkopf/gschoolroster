a1b8e95869d3cc203472fb3872bf48280f76e8b5
Blair Anderson <blair81@gmail.com>
2013-02-23 20:14:38 -0700

    adding rows for nth index result

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -12,17 +12,9 @@ module ProfilesHelper
     <i class='icon-github'></i> #{github}</a>".html_safe
   end
 
-   def hidden_email(email)
-     "#{email[0..email.index("@")]}****.com"
-   end
-
-  def hidden_phone(phone)
-    "(***)-***-#{phone[-4..-1]}"
-  end
-
   def fancy_linkedin(profile)
     a = profile.name.split
-    "<a class='text-inverse' href=#{valid_linkedin(profile.linkedin)}><i class='icon-linkedin'></i> <b>#{a[0][0].upcase}. #{a[-1].capitalize}</b></a>".html_safe
+    "<a target='_blank' class='text-inverse' href=#{valid_linkedin(profile.linkedin)}><i class='icon-linkedin'></i><b>#{a[0][0].upcase}. #{a[-1].capitalize}</b></a>".html_safe
   end
 
   def valid_linkedin(input)
@@ -34,13 +26,7 @@ module ProfilesHelper
   end
 
   def fancy_web_link(url)
-    "<a href='#{clean_link(url)}' class='text-info' ><i class='icon-bookmark text-error'>
-    <small>
-    blog:
-    </small>
-    #{pretty_url(url)}
-    </i>
-    </a>".html_safe
+    "<a target='_blank' class='text-error' href=#{clean_link(url)} ><i class='icon-bookmark text-error'></i> #{pretty_url(url)}</a>".html_safe
   end
 
   def clean_link(url)
@@ -55,6 +41,13 @@ module ProfilesHelper
     url.gsub("http://","").gsub(".herokuapp.com", "").gsub("/","")
   end
 
+   def hidden_email(email)
+     "#{email[0..email.index("@")]}****.com"
+   end
+
+  def hidden_phone(phone)
+    "(***)-***-#{phone[-4..-1]}"
+  end
 
 
 end
