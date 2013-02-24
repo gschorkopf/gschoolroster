eadc33ab95b551e7aafca7758e47d1beabe36ff2
Blair Anderson <blair81@gmail.com>
2013-02-23 19:11:18 -0700

    adding validation for blog, twitter, linkedin urls

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,4 +1,17 @@
 module ProfilesHelper
+
+  def fancy_twitter_link(twitter)
+    "<a class='text-info' href='https://twitter.com/intent/user?screen_name=#{twitter}'>
+    <i class='icon-twitter'></i> #{twitter}</a>".html_safe
+  end
+
+  def fancy_github_link(github)
+    "<a target='_blank'
+     href='https://github.com/#{github}' 
+     class='muted'>
+    <i class='icon-github'></i> #{github}</a>".html_safe
+  end
+
    def hidden_email(email)
      "#{email[0..email.index("@")]}****.com"
    end
@@ -8,31 +21,40 @@ module ProfilesHelper
   end
 
   def fancy_linkedin(profile)
-    if profile.name.split.length == 2
-      a,b = profile.name.split  
-      link_to "#{a[0].upcase}. #{b.capitalize}", "http://linkedin.com/in/#{profile.linkedin}"
+    a = profile.name.split
+    "<a class='text-inverse' href=#{valid_linkedin(profile.linkedin)}><i class='icon-linkedin'></i> <b>#{a[0][0].upcase}. #{a[-1].capitalize}</b></a>".html_safe
+  end
+
+  def valid_linkedin(input)
+    if input.include?("http")
+      input
     else
-      link_to "linkedin", "http://linkedin.com/in/#{profile.linkedin}"
+      "http://linkedin.com/in/#{input}"
     end
   end
 
   def fancy_web_link(url)
-    if url.include?("herokuapp.com")
-      link_to url.gsub(/http:\/\//,"").gsub(/.herokuapp.com/,""), url, class: "text-error"
+    "<a href='#{clean_link(url)}' class='text-info' ><i class='icon-bookmark text-error'>
+    <small>
+    blog:
+    </small>
+    #{pretty_url(url)}
+    </i>
+    </a>".html_safe
+  end
+
+  def clean_link(url)
+    if url.include?("http")
+      url
     else
-      link_to url.gsub(/http:\/\//,""), url
+      "http://#{url}"
     end
   end
 
-  def fancy_twitter_link(twitter)
-    "<a class='text-info' href='https://twitter.com/intent/user?screen_name=#{twitter}'>
-    <i class='icon-twitter'></i>#{twitter}</a>".html_safe
+  def pretty_url(url)
+    url.gsub("http://","").gsub(".herokuapp.com", "").gsub("/","")
   end
 
-  def fancy_github_link(github)
-    "<a target='_blank'
-     href='https://github.com/#{github}' 
-     class='muted'>
-    <i class='icon-github'></i>#{github}</a>".html_safe
-  end
+
+
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ActiveRecord::Base
   attr_accessible :email, :github, :name, :blog, :text_editor, :twitter, :linkedin
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :blog, :twitter, :github
+
+  # validates_format_of :blog , :with => /^(http|https)://[a-z0-9]+([-.]{1}[a-z0-9]+)* .[a-z]{2,5}(([0-9]{1,5})?/.*)?$/ix
 end
